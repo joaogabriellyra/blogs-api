dc22db56acd0923c3d900f75f27e0e02baf0d517
João Gabriel Lyra <jgabriellyra@hotmail.com>
2022-10-16 17:03:54 -0300

    requisito 7 concluído

---
+++ src/models/Category.js
@@ -0,0 +1,23 @@
+// src/models/User.js
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: { 
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false, 
+            autoIncrement: true,
+        },
+        name: { 
+            type: DataTypes.STRING,
+            allowNull: false, 
+        } 
+    },
+      {
+        timestamps: false,
+        tableName: 'categories',
+        underscored: true
+      });
+  
+    return Category;
+  };
+  
